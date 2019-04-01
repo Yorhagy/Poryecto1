fac9cda4b7f7432724d532eaac74dace915a69bf
yorhagy <yorhagyv@outlook.es>
2019-04-01 10:42:04 -0500

    Retornar JSON en index.html

--- Courses_Recom.py
+++ Courses_Recom.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 import os
 from filtro_data import DbFilter, AlS_implicit, MatrixTransformer
 from Model_Recommendation import recommentation
 import pandas as pd
+import json
 
 #dbdir = 'sqlite:///' + os.path.abspath(os.getcwd()) + '/db_comfama.csv'
 data = pd.read_csv('/home/yorhagy/Escritorio/Comfama_recommendation/db_comfama.csv', nrows=10)
@@ -15,7 +16,9 @@ def index():
     model = AlS_implicit(data_).models()
     #data_ = DbFilter(data).filter_()
     cursos = recommentation(model, data_, userId='-h-german@hotmail.com').recommendCourses()
-    return cursos.to_json(orient='records')
+    d = cursos.to_json(orient='records')
+    return render_template('index.html',courses= json.loads(d))
+    #return cursos.to_json(orient='records')
 
 if __name__ == "__main__":
     app.run(debug= True, port= 8000)
